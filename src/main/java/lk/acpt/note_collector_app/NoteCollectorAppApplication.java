cad0c586e3b16d0a6ef904ede9190f2dfc2085be
KaveeshaKodithuwakku <k.kaveesha95@gmail.com>
2023-03-29 21:05:19 +0530

    first commit

---
+++ src/main/java/lk/acpt/note_collector_app/NoteCollectorAppApplication.java
@@ -0,0 +1,13 @@
+package lk.acpt.note_collector_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCollectorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteCollectorAppApplication.class, args);
+	}
+
+}
